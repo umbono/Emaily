a4729e52cf9849617d0e02270b0272431e93e262
Ravi Tiwari <rtiwariops@gmail.com>
2019-01-25 18:38:22 -0800

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./models/user');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 const app = express();
 
-//Route Handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'EmailyApp' });
-});
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT)
